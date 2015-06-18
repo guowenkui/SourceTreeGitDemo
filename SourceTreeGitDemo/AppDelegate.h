eb9ab855265a13448a2b575719310549959bebbd
郭文魁 <982625649@qq.com>
2015-06-18 15:13:22 +0800

    Initial Commit

---
+++ SourceTreeGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceTreeGitDemo
+//
+//  Created by 郭文魁 on 15/6/18.
+//  Copyright (c) 2015年 郭文魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SourceTreeGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SourceTreeGitDemo
+//
+//  Created by 郭文魁 on 15/6/18.
+//  Copyright (c) 2015年 郭文魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
